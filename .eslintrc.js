c71813897715e0eb6908a948e87ee2ef4eadf60b
mi.____.ng <alstjr9438@naver.com>
2024-05-15 22:39:56 +0900

    chore: package setting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		"plugin:prettier/recommended",
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:storybook/recommended',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'prettier', 'simple-import-sort'],
+	rules: {
+		"prettier/prettier": "error",
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'simple-import-sort/imports': [
+			'error',
+			{
+				groups: [
+					// react 패키지
+					['^react', '^@?\\w'],
+					// components 디렉토리 내 모듈
+					['^(@|components)(/.*|$)'],
+					// 스타일 시트 파일
+					['^.+\\.?(css)$'],
+				],
+			},
+		],
+		'simple-import-sort/exports': 'error',
+	},
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-    content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
-    theme: {
-        extend: {
-            backgroundColor: {
-                pink: '#FCE8F1',
-            },
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
-        },
-    },
-    plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundColor: {
+				pink: '#FCE8F1',
+			},
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0,0,0,0.3)',
+				'2md': '0 1px 10px 0 rgba(0,0,0,0.05)',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
